e647ae09bc68336c4f8a1a81a5bdd3d7fddba0c1
nohsn <minuee@kormedi.com>
2025-05-14 17:41:31 +0900

    20250514

---
+++ rsync_my_git.sh
@@ -0,0 +1,2 @@
+rm -rf /Users/kormedi/Documents/WorkPlace/bitbucket/be.crawler3/*
+rsync -rub --delete --exclude={'node_modules','out','.git'} /Users/kormedi/Documents/WorkPlace/bitbucket/docter_crawler/* /Users/kormedi/Documents/WorkPlace/bitbucket/be.crawler3/
